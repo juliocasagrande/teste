2f20c00eab39963bd7a68a2a2de87f199713c299
Julio Lima Casagrande <JLCASAGRANDE@ctgpar.ctgbr.com.br>
2023-10-04 10:12:33 -0300

    alteracao

---
+++ 1_🏡_Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    layout='wide',
+    page_title= "Home"
+)
+
+option = st.sidebar.selectbox("Navegar para:", ("Página Inicial", "Página 2", "Página 3"))
+
+st.title("Home")
+st.header(option)
---
+++ pages/2_📄_ Documentos.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    layout='wide',
+    page_title='Pagina 2'
+)
+
+st.title('Página 2')
